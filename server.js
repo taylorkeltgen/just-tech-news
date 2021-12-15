1a349518ab2e6e0ea6c97912874be76689f6850f
taylorkeltgen <taylorkeltgen@gmail.com>
2021-12-14 20:14:13 -0600

    homepage view feature completed

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+// ==Handlebars==
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// ==Express Standard==
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
